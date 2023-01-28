9e234328fc08466530d68a8246d3b7af06281a0e
Marcelo Melo <marcellomello90@gmail.com>
2023-01-28 01:43:56 +0100

    corrigir o erro 9

---
+++ script.js
@@ -0,0 +1,13 @@
+//variables//
+const form = document.querySelect("form")
+const inputWeight = document.querySelector("#weight")
+const inputHeight = document.querySelector("#height")
+
+form.onsubmit = function(event) {
+  event.preventDefault()
+
+  const weight = inputWeight.value
+  const height = inputHeight.value
+
+  
+}
