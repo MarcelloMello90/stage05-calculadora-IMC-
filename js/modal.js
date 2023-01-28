999f6ad4a4758949f84b627c2fac8007e7218488
Marcelo Melo <marcellomello90@gmail.com>
2023-01-28 01:36:54 +0100

    creating the JavaScript file

---
+++ js/modal.js
@@ -0,0 +1,17 @@
+export const Modal = {
+  Wrapper: document.querySelector(".modal-wrapper"),
+  Message: document.querySelector(".modal .title span"),
+  ButtonClose: document.querySelector(".modal button.close"),
+
+  open () {
+    Modal.Wrapper.classList.add("open")
+  },
+  close (){
+    Modal.Wrapper.classList.remove("open")
+  }
+}
+
+
+Modal.ButtonClose.onclick = () => {
+  Modal.close()
+}
