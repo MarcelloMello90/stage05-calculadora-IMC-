14ed2096ea71f0d9fa20c19d60d3d71881a9c4e4
Marcelo Melo <marcellomello90@gmail.com>
2023-01-28 01:45:39 +0100

    modified:   js/script.js, modified:   js/utils.js

--- js/script.js
+++ js/script.js
@@ -1,11 +1,14 @@
 import { Modal } from "./modal.js"
 import {AlertError} from "./alert-error.js"
-import {IMC, notANumber} from "./utils.js"
+import {calculateIMC, notANumber} from "./utils.js"
 
 //variables//
 const form = document.querySelector("form")
 const inputWeight = document.querySelector("#weight")
 const inputHeight = document.querySelector("#height")
+
+inputWeight.oninput = () => AlertError.close()
+inputHeight.oninput = () => AlertError.close()
 
 form.onsubmit = function(event) {
   event.preventDefault()
@@ -13,19 +16,22 @@ form.onsubmit = function(event) {
   const weight = inputWeight.value
   const height = inputHeight.value
 
-  const showAlertError = notANumber(weight) || notANumber(height)
+  const weightOrHeightIsNotANumber = notANumber(weight) || notANumber(height)
 
-  if (showAlertError) {
+  if (weightOrHeightIsNotANumber) {
     AlertError.open()
     return;
   }
 
   AlertError.close()
 
-  const result = IMC(weight, height)
+  const result = calculateIMC(weight, height)
+  displayResultMessage(result)
+}
+
+function displayResultMessage(result) {
   const message = `Seu IMC é de ${result}`
 
   Modal.message.innerText = message
   Modal.open()
 }
-
